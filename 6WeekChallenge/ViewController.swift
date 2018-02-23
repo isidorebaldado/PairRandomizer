48166a809db8ccf42684feeabe9c0faaa1e67092
Isidore <isidorebaldado@knights.ucf.edu>
2018-02-23 09:22:36 -0700

    Initial Commit

---
+++ 6WeekChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  6WeekChallenge
+//
+//  Created by Isidore Baldado on 2/23/18.
+//  Copyright © 2018 Isidore Baldado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
